4d988e13f69eb2dd16a330c439630aeba6084782
Mumtaz203 <mitaserdogan@gmail.com>
2025-07-22 20:21:20 +0200

    Base structure is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import Header from './JMDB/components/Header'; // header'ı ekliyoruz
+import CreateUserForm from '@/JMDB/services/mutations/userMut';
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    HttpLink,
+    from,
+} from "@apollo/client"
+import {onError} from "@apollo/client/link/error";
+
+const erorLink=onError(({graphQLErrors}) => {
+if (graphQLErrors) {
+    graphQLErrors.map(({message}) => {
+        alert(`graphQL error: ${message}`);
+    });
+}
+});
+
+
+const link=from([
+    erorLink,
+    new HttpLink({uri:'http://localhost:8080/graphql',})
+]);
+
+
+const client=new ApolloClient({
+cache: new InMemoryCache(),
+    link: link,
+});
+
 
 function App() {
     return (
-        <div>
-            <Header />
-            {/* Sayfa içeriği burada olacak */}
-            <main style={{ padding: '2rem' }}>
-                <h2>Welcome to JMDB!</h2>
-            </main>
-        </div>
+
+    <div>
+        <ApolloProvider client={client}></ApolloProvider>
+        <h1>Kullanıcı Oluştur</h1>
+        <CreateUserForm />
+    </div>
     );
 }
 
